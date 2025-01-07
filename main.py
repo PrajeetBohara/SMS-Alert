06cd2005ede3c053ccb3fcb771eb040a1b65601b
Prajeet Bohara <153901646+PrajeetBohara@users.noreply.github.com>
2025-01-06 21:01:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-#website used openweathermap.org to use api keys
-#apikey = cb2afbc18c170c7c998c003a20f3258b
 
+from twilio.rest import Client
 import requests
 
-api_key = "cb2afbc18c170c7c998c003a20f3258b"
+api_key = "your API key for weather app"
+account_sid = "Twilio account sid" #for twilio
+auth_token = "twilio account authorization token" #for twilio
+message = "Any message"
 
 request = requests.get("https://api.openweathermap.org/data/2.5/forecast?lat=30.212942&lon=-93.218910&cnt=4&appid=cb2afbc18c170c7c998c003a20f3258b")
 request.raise_for_status()
@@ -18,6 +20,14 @@ for i in json_file["list"]:
         will_it_rain = True
 
 if will_it_rain:
-    print("Bring an Umbrella")
+    client = Client(account_sid, auth_token)
 
-#MQK5T4EK43BY49F2AUUPT626
+    message = client.messages.create(
+        body=message,
+        from_="+1sender phone number",
+        to="+1receiver phone number",
+    )
+    print(f"Message sent: {message}")
+
+
+
